03eefc7a34472597175709009e0c50d4495a6c69
“Lucero <lhernandez392@insite.4cd.edu  git config --global user.name Lucero Hernandez  git config --global user.email lhernandez392@insite.4cd.edu>
2023-05-04 21:04:41 -0700

    add map and updated the sizing

--- main.js
+++ main.js
@@ -12,4 +12,12 @@ fetch('https://dog.ceo/api/breeds/image/random')
     const pic = `<img src="${img}">`;
     document.getElementById("dog").innerHTML = pic;
   };
-     //dog pic
+     //dog park map
+
+     var map = L.map('map').setView([37.98415, -122.02766], 13);
+     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+var marker = L.marker([37.98415, -122.02766],).addTo(map);
+marker.bindPopup("<b>Baldwin Dog Park!</b><br>2750 Parkside Cir, Concord, CA 94519.").openPopup();
