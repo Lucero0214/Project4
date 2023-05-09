35d09d3839c924d490df3a742692e80ced29280e
“Lucero <lhernandez392@insite.4cd.edu  git config --global user.name Lucero Hernandez  git config --global user.email lhernandez392@insite.4cd.edu>
2023-05-09 11:28:58 -0700

    attempted to add drop down

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ fetch('https://dog.ceo/api/breeds/image/random')
     const pic = `<img src="${img}">`;
     document.getElementById("dog").innerHTML = pic;
   };
+
      //dog park map
 
      var map = L.map('map').setView([37.98415, -122.02766], 13);
@@ -20,4 +21,4 @@ fetch('https://dog.ceo/api/breeds/image/random')
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 var marker = L.marker([37.98415, -122.02766],).addTo(map);
-marker.bindPopup("<b>Baldwin Dog Park!</b><br>2750 Parkside Cir, Concord, CA 94519.").openPopup();
+marker.bindPopup("<b>Baldwin Dog Park!</b><br>2750 Parkside Cir, Concord, CA 94519.").openPopup()
