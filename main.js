b94a6fe4c560ec683cd452f26e02547f01390f1f
“Lucero <lhernandez392@insite.4cd.edu  git config --global user.name Lucero Hernandez  git config --global user.email lhernandez392@insite.4cd.edu>
2023-05-17 20:26:08 -0700

    set up the button image function

--- main.js
+++ main.js
@@ -127,5 +127,16 @@ if (code === "SF") {
     } // end if/else
   }); //end addEventListener - change
 }
+const dogImage = document.getElementById('dog-img');
+const dogButton = document.getElementById('btn-dog');
+dogButton.addEventListener('click', fetchDogImage);
 
-
+function fetchDogImage() {
+  fetch('https://dog.ceo/api/breeds/image/random')
+  .then((response) => response.json())
+  .then(data => {
+    let img = data.message;
+    displayDog(img);
+    console.log(data)
+  })
+}
